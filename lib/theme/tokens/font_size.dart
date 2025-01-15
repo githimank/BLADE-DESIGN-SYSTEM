daec4143f9e4405897a248a8ecbba7f0163cbefa
himankBellpost <himank@bellpost.com>
2025-01-15 23:15:08 +0530

    added fontSize & fontSizeScale

---
+++ lib/theme/tokens/font_size.dart
@@ -0,0 +1,20 @@
+class GFontSize {
+  static const double globalFz00 = 0;
+  static const double globalFz10 = 10;
+  static const double globalFz11 = 11;
+  static const double globalFz12 = 12;
+  static const double globalFz14 = 14;
+  static const double globalFz16 = 16;
+  static const double globalFz18 = 18;
+  static const double globalFz20 = 20;
+  static const double globalFz24 = 24;
+  static const double globalFz32 = 32;
+  static const double globalFz34 = 34;
+  static const double globalFz36 = 36;
+  static const double globalFz38 = 38;
+  static const double globalFz40 = 40;
+  static const double globalFz48 = 48;
+  static const double globalFz56 = 56;
+  static const double globalFz64 = 64;
+  static const double globalFz72 = 72;
+}
