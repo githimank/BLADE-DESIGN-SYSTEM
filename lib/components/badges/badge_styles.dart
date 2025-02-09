b8ec3e9cc91c5cffafc059d4dec59142eaa145e2
himankBellpost <himank@bellpost.com>
2025-02-09 22:55:56 +0530

    badge component aded

---
+++ lib/components/badges/badge_styles.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:mortars_design_system/components/badges/bp_badges.dart';
+import 'package:mortars_design_system/theme/theme_extensions.dart';
+
+class BadgePropertyColors {
+  final Color background;
+  final Color text;
+  final Color icon;
+
+  const BadgePropertyColors({
+    required this.background,
+    required this.text,
+    required this.icon,
+  });
+}
+
+class BadgeStateColors {
+  final BadgePropertyColors defaultColor;
+
+  const BadgeStateColors({
+    required this.defaultColor,
+  });
+}
+
+class BadgeProperties {
+  final BadgeStateColors primary;
+  final BadgeStateColors negative;
+  final BadgeStateColors positive;
+  final BadgeStateColors notice;
+  final BadgeStateColors information;
+  final BadgeStateColors neutral;
+
+  BadgeProperties({
+    required this.primary,
+    required this.negative,
+    required this.positive,
+    required this.notice,
+    required this.information,
+    required this.neutral,
+  });
+
+  BadgeStateColors stateColors(BadgeMood mood) {
+    return switch (mood) {
+      BadgeMood.PRIMARY => primary,
+      BadgeMood.NEGATIVE => negative,
+      BadgeMood.POSITIVE => positive,
+      BadgeMood.NOTICE => notice,
+      BadgeMood.INFORMATION => information,
+      BadgeMood.NEUTRAL => neutral
+    };
+  }
+
+  static BadgeProperties subtle(BuildContext context) => BadgeProperties(
+        positive: BadgeStateColors(
+          defaultColor: BadgePropertyColors(
+            background: context.colorTheme.feedback.background.positive.subtle,
+            text: context.colorTheme.feedback.text.positive.intense,
+            icon: context.colorTheme.feedback.icon.positive.intense,
+          ),
+        ),
+        negative: BadgeStateColors(
+          defaultColor: BadgePropertyColors(
+            background: context.colorTheme.feedback.background.negative.subtle,
+            text: context.colorTheme.feedback.text.negative.intense,
+            icon: context.colorTheme.feedback.icon.negative.intense,
+          ),
+        ),
+        notice: BadgeStateColors(
+          defaultColor: BadgePropertyColors(
+            background: context.colorTheme.feedback.background.notice.subtle,
+            text: context.colorTheme.feedback.text.notice.intense,
+            icon: context.colorTheme.feedback.icon.notice.intense,
+          ),
+        ),
+        information: BadgeStateColors(
+          defaultColor: BadgePropertyColors(
+            background: context.colorTheme.feedback.background.information.subtle,
+            text: context.colorTheme.feedback.text.information.intense,
+            icon: context.colorTheme.feedback.icon.information.intense,
+          ),
+        ),
+        neutral: BadgeStateColors(
+          defaultColor: BadgePropertyColors(
+            background: context.colorTheme.feedback.background.neutral.subtle,
+            text: context.colorTheme.feedback.text.neutral.intense,
+            icon: context.colorTheme.feedback.icon.neutral.intense,
+          ),
+        ),
+        primary: BadgeStateColors(
+          defaultColor: BadgePropertyColors(
+            background: context.colorTheme.surface.background.primary.subtle,
+            text: context.colorTheme.surface.text.primary.normal,
+            icon: context.colorTheme.surface.icon.primary.normal,
+          ),
+        ),
+      );
+
+  static BadgeProperties intense(BuildContext context) => BadgeProperties(
+    positive: BadgeStateColors(
+      defaultColor: BadgePropertyColors(
+        background: context.colorTheme.feedback.background.positive.intense,
+        text: context.colorTheme.surface.text.staticWhite.normal,
+        icon: context.colorTheme.surface.icon.staticWhite.normal,
+      ),
+    ),
+    negative: BadgeStateColors(
+      defaultColor: BadgePropertyColors(
+        background: context.colorTheme.feedback.background.negative.intense,
+        text: context.colorTheme.surface.text.staticWhite.normal,
+        icon: context.colorTheme.surface.icon.staticWhite.normal,
+      ),
+    ),
+    notice: BadgeStateColors(
+      defaultColor: BadgePropertyColors(
+        background: context.colorTheme.feedback.background.notice.intense,
+        text: context.colorTheme.surface.text.staticWhite.normal,
+        icon: context.colorTheme.surface.icon.staticWhite.normal,
+      ),
+    ),
+    information: BadgeStateColors(
+      defaultColor: BadgePropertyColors(
+        background: context.colorTheme.feedback.background.information.intense,
+        text: context.colorTheme.surface.text.staticWhite.normal,
+        icon: context.colorTheme.surface.icon.staticWhite.normal,
+      ),
+    ),
+    neutral: BadgeStateColors(
+      defaultColor: BadgePropertyColors(
+        background: context.colorTheme.feedback.background.neutral.intense,
+        text: context.colorTheme.surface.text.staticWhite.normal,
+        icon: context.colorTheme.surface.icon.staticWhite.normal,
+      ),
+    ),
+    primary: BadgeStateColors(
+      defaultColor: BadgePropertyColors(
+        background: context.colorTheme.surface.background.primary.intense,
+        text: context.colorTheme.surface.text.staticWhite.normal,
+        icon: context.colorTheme.surface.icon.staticWhite.normal,
+      ),
+    ),
+  );
+}
---
+++ lib/components/badges/bp_badges.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:mortars_design_system/components/badges/badge_styles.dart';
+import 'package:mortars_design_system/theme/theme_extensions.dart';
+import 'package:mortars_design_system/theme/tokens/spacing.dart';
+
+enum BadgeEmphasis {
+  SUBTLE,
+  INTENSE,
+}
+
+enum BadgeSize {
+  LARGE(24.0, Spacings.spacing4),
+  MEDIUM(20.0, Spacings.spacing3),
+  SMALL(16.0, Spacings.spacing2);
+
+  final double height;
+  final double horizontalPadding;
+
+  const BadgeSize(this.height, this.horizontalPadding);
+
+  TextStyle textStyle(BuildContext context) => switch (this) {
+        BadgeSize.LARGE => context.appTypography.bodySmall.medium!,
+        BadgeSize.MEDIUM => context.appTypography.bodySmall.medium!,
+        BadgeSize.SMALL => context.appTypography.bodyXSmall.medium!,
+      };
+}
+
+enum BadgeMood {
+  POSITIVE,
+  NEGATIVE,
+  NOTICE,
+  INFORMATION,
+  NEUTRAL,
+  PRIMARY;
+}
+
+class BpBadges extends StatelessWidget {
+  final BadgeEmphasis emphasis;
+  final BadgeMood mood;
+  final BadgeSize size;
+  final bool showLeadingIcon;
+  final Widget? icon;
+  final String label;
+
+  const BpBadges(
+      {this.emphasis = BadgeEmphasis.SUBTLE,
+      this.mood = BadgeMood.POSITIVE,
+      this.size = BadgeSize.MEDIUM,
+      required this.label,
+      this.showLeadingIcon = false,
+      this.icon,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final BadgeProperties badgeProperties = switch (emphasis) {
+      BadgeEmphasis.INTENSE => BadgeProperties.intense(context),
+      BadgeEmphasis.SUBTLE => BadgeProperties.subtle(context),
+    };
+
+    final BadgeStateColors badgeTheme = badgeProperties.stateColors(mood);
+
+    return Container(
+      height: size.height,
+      padding: EdgeInsets.symmetric(
+        horizontal: size.horizontalPadding,
+      ),
+      decoration: BoxDecoration(
+        color: badgeTheme.defaultColor.background,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(12.0),
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (showLeadingIcon && icon != null) ...[
+            IconTheme(
+              data: IconThemeData(
+                  color: badgeTheme.defaultColor.icon, size: Spacings.spacing5),
+              child: icon!,
+            ),
+            Spacings.spacing3.wBox,
+          ],
+          Flexible(
+            child: Text(
+              label,
+              overflow: TextOverflow.ellipsis,
+              style: size
+                  .textStyle(context)
+                  .copyWith(color: badgeTheme.defaultColor.text),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
